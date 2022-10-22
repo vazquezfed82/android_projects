76a18fa188d6512b882959b3a7350a3d493c76f9
Federico Vazquez <federicoarielvazquez@gmail.com>
2022-10-22 16:33:57 -0300

    navigation terminado

--- TestingNavigation/app/src/main/java/com/example/testingnavigation/FirstNavFragment.kt
+++ TestingNavigation/app/src/main/java/com/example/testingnavigation/FirstNavFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,15 @@ class FirstNavFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first_nav, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navigateButton = view.findViewById<Button>(R.id.button_next_fragment)
+        navigateButton.setOnClickListener {
+            findNavController().navigate(R.id.action_firstNavFragment_to_secondNavFragment)
+        }
+
     }
 
     companion object {
